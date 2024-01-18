d56e5332aba21b9ca8efa91e7c7357696b9c6b74
mogami <ndarfc.57@gmail.com>
2024-01-19 00:46:47 +0900

    [add]とりあえずページとヘッダーフッターを作ってみた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @user = User.find(params[:id])
-    end
-
-    def new
-        @user = User.new
-    end
-
-    @user = User.new(user_params)
-    if @user.save
-        auto_login(@user)
-        redirect_to '/dashbord', success: 'ユーザー登録が完了しました'
-    else
-        render :new, status: :unprocessable_entity
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email)
+        
     end
 end
