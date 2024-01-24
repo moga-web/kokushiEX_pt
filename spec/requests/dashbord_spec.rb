99f92573a2444e79dab27b206919b8d48cfededb
mogami <ndarfc.57@gmail.com>
2024-01-24 23:31:24 +0900

    [fix]requests/dashbordの対象ルートを修正

--- spec/requests/dashbord_spec.rb
+++ spec/requests/dashbord_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Dashbords", type: :request do
-  describe "GET /index" do
+  describe "GET /dashbord" do
     it "returns http success" do
-      get "/dashbord/index"
+      get "/dashbord"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
