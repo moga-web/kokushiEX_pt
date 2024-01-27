244e63ac1ee442eeaf4585a92d27062103f33128
mogami <ndarfc.57@gmail.com>
2024-01-27 19:33:34 +0900

    [fix]githubのworkflowsにPrecompile assetsを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.html.erb", "./app/helpers/**/*.rb", "./app/javascript/**/*.js"],
   theme: {
     extend: {},
   },
