88cf019fbe16d19838c37d79fd7947fab2ad6a60
mogami <ndarfc.57@gmail.com>
2024-01-29 04:47:45 +0900

    [add]topページのリンクを活性化しスタイルを適用

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.html.erb", "./app/helpers/**/*.rb", "./app/javascript/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
