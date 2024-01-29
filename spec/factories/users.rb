0ebc6a03af00199107418b7b0504dd7b96c798de
mogami <ndarfc.57@gmail.com>
2024-01-29 21:09:11 +0900

    [add]Userモデルにバリデーションを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  unconfirmed_email      :string
+#  username               :string           not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     
