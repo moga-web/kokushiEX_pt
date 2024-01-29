371b34f08ac2536bcba03f54201368ac22345b2c
mogami <ndarfc.57@gmail.com>
2024-01-29 22:58:42 +0900

    [fix]deviseのログイン・サインアップ後のリダイレクト先を変更

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /users/sign_up" do
+    it "ユーザー登録画面が表示される" do
+      get new_user_registration_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /users/sign_up" do
+    context '正常系' do
+      it 'ユーザー登録ができる' do
+        user_params = { user: { username: 'newuser', email: 'newuser@example.com', password: 'password', password_confirmation: 'password' } }
+        expect {
+          post user_registration_path, params: user_params
+        }.to change(User, :count).by(1)
+        expect(response).to redirect_to(dashbord_path) 
+      end
+    end
+
+    context '不正なパラメータの場合' do
+      it 'ユーザー登録ができない' do
+        user_params = { user: { username: '', email: 'user@example.com', password: 'password', password_confirmation: 'password' } }
+        expect {
+          post user_registration_path, params: user_params
+        }.not_to change(User, :count)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET /users/sign_in" do
+    it "ログイン画面が表示される" do
+      get new_user_session_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /users/sign_in" do
+    # ここでユーザーを作成
+    let!(:user) { create(:user, email: 'user@example.com', password: 'password') }
+
+    context '正常系' do
+      it 'ログインができる' do
+        post user_session_path, params: { user: { email: 'user@example.com', password: 'password' } }
+        expect(response).to redirect_to(dashbord_path)
+      end
+    end
+
+    context '不正なパラメータの場合' do
+      it 'ログインができない' do
+        post user_session_path, params: { user: { email: 'user@example.com', password: 'incorrect' } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET /users/edit" do
+    it "ユーザー編集画面が表示される" do
+      get edit_user_registration_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH /users/edit" do
+    it "ユーザー情報が更新される" do
+      user = create(:user)
+      login_as(user)
+      patch user_registration_path, params: { user: { email: 'updated@example.com' } }
+      expect(response).to redirect_to(edit_user_registration_path) 
+      expect(user.reload.email).to eq 'updated@example.com'
+    end
   end
 end
+
