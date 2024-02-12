4739d825a3a8c1b8d23215db87ace172a2040e64
mogami <ndarfc.57@gmail.com>
2024-02-12 12:07:01 +0900

    [fix]モデルスペックの記述要領を変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,30 +32,32 @@ RSpec.describe User, type: :model do
   end
 
   context 'バリデーション' do
-    it 'ユーザー名が空だと登録できない' do
-      user.username = nil
-      expect(user).not_to be_valid
+    context 'ユーザー名が空だと登録できない' do
+      subject(:user) { build(:user, username: nil) }
+      it {is_expected.to be_invalid}
     end
 
-    it 'メールアドレスが空だと登録できない' do
-      user.email = nil
-      expect(user).not_to be_valid
+    context 'メールアドレスが空だと登録できない' do
+      subject(:user) { build(:user, email: nil) }
+      it {is_expected.to be_invalid}
     end
 
-    it 'メールアドレスが重複していると登録できない' do
-      user.save
-      another_user = build(:user, email: user.email)
-      expect(another_user).not_to be_valid
+    context 'メールアドレスが重複している場合' do
+      subject(:user) { create(:user) }
+      it '登録できない' do
+        another_user = build(:user, email: user.email)
+        expect(another_user).to be_invalid
+      end
     end
 
-    it 'パスワードが空だと登録できない' do
-      user.password = nil
-      expect(user).not_to be_valid
+    context 'パスワードが空だと登録できない' do
+      subject(:user) { build(:user, password: nil) }
+      it {is_expected.to be_invalid}
     end
 
-    it 'パスワードが6文字未満だと登録できない' do
-      user.password = 'abc'
-      expect(user).not_to be_valid
+    context 'パスワードが6文字未満だと登録できない' do
+      subject(:user) { build(:user, password: 'abc') }
+      it {is_expected.to be_invalid}
     end
   end
 end
