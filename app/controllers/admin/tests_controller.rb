5586909181400771954f34562cf734ac778fb099
mogami <ndarfc.57@gmail.com>
2024-03-05 10:24:23 +0900

    [add]seed_fuをインストール

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,11 @@
+require 'csv'
+class Admin::TestsController < ApplicationController
+    def index
+        @tests = Test.all
+    end
+        
+    def import
+        Test.import(params[:file])
+        redirect_to root_url, notice: 'tests imported.'
+    end
+end
